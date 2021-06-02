382851e6225a6f7f69898cbe3bff3ecb11f0d84c
shubhamrade <shubham.rade@msm.digital>
2021-06-02 09:54:52 +0530

    updated automation script

--- automation.sh
+++ automation.sh
@@ -42,10 +42,13 @@ echo "----------------------------------------------------"
 echo "creating tar File for all log file "
 timestamp=$(date '+%d%m%Y-%H%M%S')
 log_file_name=*.log
-tar_file_name=$my_name-httpd-logs-$timestamp.tar
+log_type=httpd-logs
+file_type=tar
+tar_file_name=$my_name-$log_type-$timestamp.$file_type
 tar_file_path=/tmp/$tar_file_name
 
 cd /var/log/apache2/
+
 tar -cvf $tar_file_path $log_file_name
 
 
@@ -59,4 +62,19 @@ s3://$s3_bucket/$tar_file_name
 
 echo "file: $tar_file_name is uploaded on s3 server"
 echo "----------------------------------------------------"
+
+
+file_size=`du -h "$tar_file_path" | cut -f1`
+echo "----------------------------------------------------"
+file="/var/www/html/inventory.html"
+if [ -f "$file" ]
+then
+        echo "$file is already created"
+else
+        touch $file
+        echo "$file is created"
+        echo " <p> <b> Log Type  &emsp;   Date Created   &emsp; &emsp;    Type   &emsp;   Size </b> </p> " >>$file
+fi
+
+echo " <p> $log_type  &emsp; $timestamp  &emsp; $file_type  &emsp; $file_size </p> " >>$file
 
